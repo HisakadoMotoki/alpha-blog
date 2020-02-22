d11c25318f69c7dc92bbf3b051a73c13be96ae44
HisakadoMotoki <sagamihara71@gmail.com>
2020-02-22 09:57:38 +0900

    rextract rebundancy and prepara for proroduction

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+before_action :set_article, only: [:edit, :update, :show, :destroy]
 
 def new
   @article = Article.new
@@ -9,18 +10,15 @@ def index
 end
 
 def edit
-  @article = Article.find(params[:id])
 end
 
 def destroy
-  @article = Article.find(params[:id])
   @article.destroy
 flash[:notice] = "Article was deleted"
   redirect_to articles_path
 end
 
 def update
-  @article = Article.find(params[:id])
   if @article.update(article_params)
     flash[:notice] = "Article was successfilly updated"
     redirect_to article_path(@article)
@@ -42,7 +40,6 @@ def create
 end
 
 def show
-  @article = Article.find(params[:id])
 end
 
 private
@@ -50,5 +47,8 @@ def article_params
   params.require(:article).permit(:title, :description)
 end
 
+def set_article
+  @article = Article.find(params[:id])
+end
 
 end
