1e5fc53f2f373dd84a2e67714b760c8aaabf7c22
HisakadoMotoki <sagamihara71@gmail.com>
2020-02-24 16:54:57 +0900

    add admin delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -54,7 +54,7 @@ def set_article
 end
 
 def require_same_user
-  if current_user != @article.user
+  if current_user != @article.user and !current_user.admin?
     flash[:danger] = "you can only affect your own article"
     redirect_to root_path
   end
