cc9c38b5a353e3c9d25002b41699943d1a0d911b
HisakadoMotoki <sagamihara71@gmail.com>
2020-02-24 12:37:07 +0900

    add authenticate

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
+
   root 'pages#home'
   get 'about', to: "pages#about"
 
   resources :articles
 
   get 'signup', to: "users#new"
-  post 'users', to: "users#create"
-
+  #post 'users', to: "users#create"
   resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
