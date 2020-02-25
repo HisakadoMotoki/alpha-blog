5ee9d3a8fbd488315058da891315a6f246a7dd7a
HisakadoMotoki <sagamihara71@gmail.com>
2020-02-26 06:51:21 +0900

    arrange

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,6 @@
 class ApplicationController < ActionController::Base
-  class MyLogger < Logger
-      include LoggerSilence
-      include ActiveSupport::LoggerThreadSafeLevel
-    end
 
-    before_action :mylogger_test
-
-    private
-
-    def mylogger_test
-      mylogger = MyLogger.new(STDOUT)
-      mylogger.silence do
-        mylogger.debug("controller = #{controller_name}")
-        mylogger.info("action = #{action_name}")
-        mylogger.error("controller#action = #{controller_name}##{action_name}")
-      end
-    end
-
-
-  protect_from_forgery with: :exception
-
+protect_from_forgery with: :exception
 helper_method :current_user, :logged_in?
 
   def  current_user
