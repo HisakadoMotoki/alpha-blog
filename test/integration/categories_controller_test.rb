075a734d57965c8fc2ba06d6d5da3c285c2c2c4a
HisakadoMotoki <sagamihara71@gmail.com>
2020-02-28 07:11:49 +0900

    add category

--- test/integration/categories_controller_test.rb
+++ test/integration/categories_controller_test.rb
@@ -1,13 +1,15 @@
-require 'test_helper'
-class CreateCategoriesTest < ActionDispatch::IntegrationTest
-test "get new category form and create category" do
-get new_category_path
-assert_template 'categories/new'
-assert_difference 'Category.count', 1 do
-post_via_redirect categories_path, category: {name: "sports"}
-# if using Rails 5 use below 2 lines instead of line above (without the comment tag of course):
-# post categories_path, params: { category: { name: "sports" } }
-# follow_redirect!
-end
-end
+class CategoriesController < ApplicationController
+  def index
+    
+  end
+  def new
+    @category = Category.new
+  end
+  def create
+    
+  end
+  def show
+    
+  end
+
 end
