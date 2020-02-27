fedfc1148c741b816681c7d020b9cd6065f03e15
HisakadoMotoki <sagamihara71@gmail.com>
2020-02-28 07:58:44 +0900

    add category

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -4,18 +4,25 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
 
   def setup
     @category = Category.create(name: "sports")
+    @user = User.create(username: "motoki", email: "saga@gmail.com", password: "passward", admin: true)
   end
   test "should get categories index" do
     get :index
     expect(response).to be_success
   end
-    test "should get new" do
+  test "should get new" do
+    sign_in_as(@user, "password")
     get :new
     expect(response).to be_success
   end
-    test "should get show" do
+  test "should get show" do
     get(:show, {'id' => @category.id})
     assert_response :success
   end
-
+  test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post categories_path, params: {category: { name: "sports"}}
+    end
+    assert_redirected_to categories_path
+  end
 end
