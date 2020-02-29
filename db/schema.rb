d9abae028ef136b3d433c62303b141817c538f6b
HisakadoMotoki <sagamihara71@gmail.com>
2020-02-29 14:52:50 +0900

    arrange

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_24_090213) do
+ActiveRecord::Schema.define(version: 2020_02_29_054704) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "categories", force: :cascade do |t|
